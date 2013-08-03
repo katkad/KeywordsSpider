265a98f881df2e1200b29821aa3c591b46eb88cb
katkad <durechova@ynet.sk>
2013-08-03 18:11:48 -0400

    Initial commit

---
+++ lib/KeywordsSpider.pm
@@ -0,0 +1,209 @@
+package KeywordsSpider;
+
+use Spider 'find_origin';
+use Modern::Perl;
+use Parallel::ForkManager;
+use Getopt::Long;
+use Data::Dumper;
+
+my @unwanted_params = qw/
+  referer
+  ref
+  sesid
+  hash
+  ssid
+/;
+
+my $spidered_websites = 0;
+my %links = ();
+
+my $old_origin = '';
+my $old_origin_domain = '^$';
+
+my $COUNT = 0;
+
+sub _normalize_url {
+  my $url = shift;
+
+  chomp($url);
+  $url =~ s/^\'//g;
+  $url =~ s/\'$//g;
+
+  if ( $url !~ /http/ ) {
+    $url = 'http://' . $url;
+  }
+
+  return $url;
+}
+
+sub _remove_garbage {
+  my $referrer = shift;
+  foreach (@unwanted_params) {
+    if ( $referrer =~ /$_/ ) {
+      my $temp = $_;
+      $referrer =~ s/${temp}=.*&//g;
+      $referrer =~ s/${temp}=.*$//g;
+    }
+  }
+
+  #probably a hash
+  $referrer =~ s/=.{32}&/=&/g;
+  $referrer =~ s/=.{32}$/=/g;
+
+  return $referrer;
+}
+
+sub _get_domain {
+  my $domain = shift;
+
+  $domain =~ s/http(s)?:\/\///;
+  $domain =~ s/\'//g;
+  $domain =~ s/www\.//;
+  $domain =~ s/\/.*$//;
+
+  return $domain;
+}
+
+sub _get_initial_origin_domain {
+  my ($line) = @_;
+
+  my ($website) = split(/,/, $line);
+
+  $website = _normalize_url($website);
+  my($origin, $origin_domain) = find_origin($website);
+
+  return $origin_domain;
+}
+
+sub load_keywords {
+  my($keyfile) = shift;
+
+  open KEYWORDS, "<$keyfile" or die "problem opening file \'$keyfile\' for reading\n$!";
+  my @keywords = <KEYWORDS>;
+  close(KEYWORDS);
+
+  foreach (@keywords) {
+    $_ =~ s/[\r\n]+//g;
+  }
+
+  return @keywords;
+}
+
+my $pm = Parallel::ForkManager->new(10);
+$pm->run_on_finish( sub {
+    my ($pid, $exit_code) = @_;
+    $COUNT += $exit_code;
+});
+
+sub run {
+  my %args = @_;
+
+  my $skip_ref_regexp = $args{skip_ref_regexp} // '^http:\/\/null$';
+  my $infile = $args{infile} // 'export';
+  my $outfile = $args{outfile} // 'output.log';
+  my $keyfile = $args{keyfile} // 'keywords';
+  my $allowed_keywords_file = $args{allowed_keywords} // '';
+  my $debug = $args{debug} // 0;
+
+  open FILE, "<$infile" or die "problem opening file \'$infile\' for reading\n$!";
+  my @input = <FILE>;
+  close(FILE);
+
+  my @keywords = load_keywords($keyfile);
+
+  my @allowed_keywords_arr = ();
+  if ($allowed_keywords_file) {
+    @allowed_keywords_arr = load_keywords($allowed_keywords_file);
+  }
+
+  my %allowed_keywords = map { $_ => 1 } @allowed_keywords_arr;
+
+  my $fh;
+  open $fh, ">$outfile" or die "problem opening file \'$outfile\' for writing\n$!";
+  select($fh);
+  $| = 1;
+  binmode $fh, ":utf8";
+
+  select(STDOUT);
+  binmode STDOUT, ":utf8";
+
+  my $db_records_amount = scalar @input;
+
+  my $first_line = $input[0];
+  $old_origin_domain = _get_initial_origin_domain($first_line);
+
+  foreach (@input) {
+    my ($website, $referrer) = split(/,/, $_);
+
+    print $fh "DB: $website $referrer\n";
+
+    $website = _normalize_url($website);
+    $referrer = _normalize_url($referrer);
+
+    my ($origin, $origin_domain) = find_origin($website);
+
+    if ( $origin_domain !~ /${old_origin_domain}/i ) {
+#      print Dumper \%links;
+      $spidered_websites++;
+
+      $pm->start and goto CONTINUE;
+
+      my $spider = Spider->new(output_file => $fh, links => \%links,
+        keywords => \@keywords, allowed_keywords => \%allowed_keywords);
+      my $count = $spider->spider_links();
+
+#      my $count = spider_links(%links);
+
+      $pm->finish($count);
+
+CONTINUE:
+      %links = ();
+    }
+
+    if ($website =~ / /) {
+      foreach my $website_to_add (split(/ /, $website)) {
+        $website_to_add = _normalize_url($website_to_add);
+        $links{$website_to_add} = { depth => 0, want_spider => 1 };
+      }
+    }
+    else {
+      $links{$website} = { depth => 0, want_spider => 1 };
+    }
+    print "\n";
+
+    if ( $referrer !~ /${skip_ref_regexp}/ ) {
+      $referrer = _remove_garbage($referrer);
+
+      $links{$referrer} = { depth => 0 };
+
+      my $domain = _get_domain($website);
+
+      if ( $referrer !~ /$domain/i ) {
+        print $fh "NOT matching referrer=$referrer, website=$website\n";
+      }
+    }
+
+    $old_origin = $origin;
+    $old_origin_domain = $origin_domain;
+  }
+
+#  print Dumper \%links;
+  $spidered_websites++;
+  $pm->start and goto END; # do the fork
+
+  my $spider = Spider->new(output_file => $fh, links => \%links,
+    keywords => \@keywords, allowed_keywords => \%allowed_keywords);
+  my $count = $spider->spider_links();
+
+  $pm->finish($count);
+
+END:
+  print "waiting for children\n";
+  $pm->wait_all_children;
+
+  print $fh "number of DB records: $db_records_amount\n";
+  print $fh "number of spidered websites: $spidered_websites\n";
+  print $fh "number of counted websites: $COUNT\n";
+
+  close($fh);
+}
---
+++ lib/Spider.pm
@@ -0,0 +1,411 @@
+package Spider;
+
+use Modern::Perl;
+use Moose;
+use Data::Dumper;
+use LWP::UserAgent;
+use HTTP::Request::Common qw(GET);
+use HTTP::Cookies;
+use HTML::TreeBuilder;
+
+use base qw/ Exporter /;
+our @EXPORT = qw/ find_origin /;
+
+has [qw/output_file links/] => (
+  is => 'rw',
+  required => 1,
+);
+
+has [qw/keywords allowed_keywords/] => (
+  is => 'ro',
+  required => 1,
+);
+
+has 'output' => (
+  traits => ['String'],
+  is => 'rw',
+  isa => 'Str',
+  default => '',
+  handles => {
+    add_text     => 'append',
+  },
+);
+
+has 'counted' => (
+  is => 'rw',
+  default => 0,
+);
+
+has [qw/website origin origin_domain root alerted/] => (
+  is => 'rw',
+);
+
+$ENV{PERL_LWP_SSL_VERIFY_HOSTNAME} = 0;
+
+my $ua = LWP::UserAgent->new;
+$ua->agent('Mozilla/8.0');
+$ua->cookie_jar(
+  HTTP::Cookies->new(
+    file => 'mycookies.txt',
+    autosave => 1
+  )
+);
+$ua->timeout(15);
+$ua->max_size(512*1024);
+
+sub is_already_crawled {
+  my ($self) = @_;
+
+#  print Dumper \$self->links;
+  foreach (keys %{$self->links}) {
+    # max depth in links hash can be 1 now
+    if ($self->links->{$_}{depth} == 1 && not defined $self->links->{$_}{fetched}) {
+      return 0;
+    }
+  }
+
+  return 1;
+}
+
+sub find_origin {
+  my ($url) = @_;
+
+  my $origin = $url;
+  $origin =~ s/https?:\/\///;
+  $origin =~ s/\/.*$//;
+
+  my $domain = $origin;
+  $domain =~ s/www\.//;
+
+  return ($origin, $domain);
+}
+
+sub get_root {
+  my ($self, $url) = @_;
+
+  my $req = GET $url;
+
+  my $res = $ua->request($req);
+
+  if ($res->is_success) {
+    if (
+      ($res->header('Content-Type') =~ /^text\/html/ ||
+        $res->header('Content-Type') =~ /xml$/ ) &&
+      ((not defined $res->header('Content-Length')) ||
+        $res->header('Content-Length') < 512*1024)
+    ) {
+      my $content = $res->content;
+
+      $self->root(HTML::TreeBuilder->new_from_content($content));
+
+    } else{
+      $self->add_text('SKIPPING because of content type or length' . "\n");
+    }
+  } else {
+      $self->add_text('ERROR:' . $res->status_line . "\n");
+  }
+}
+
+
+sub get_content_with_meta_keywords {
+  my ($self) = @_;
+
+  my $meta_keywords =
+    (defined $self->root->find_by_attribute('name', 'keywords')) ?
+      $self->root->find_by_attribute('name', 'keywords')->attr('content') : '';
+
+  my $content_with_meta_keywords = $self->root->as_text . $meta_keywords;
+
+  return $content_with_meta_keywords;
+}
+
+sub _add_new_link {
+  my ($self, $link, $depth) = @_;
+  if (not exists $self->links->{$link}) {
+    $self->links->{$link} = {
+      depth => $depth,
+    };
+  }
+
+  return;
+}
+
+sub _handle_non_http_link {
+  my ($self, $link, $base, $depth) = @_;
+
+  my $href;
+
+  if ($link =~ /^\//) {
+    $href = "http://" . $self->origin . $link;
+  }
+  elsif ($link =~ /^\.\.\//) {
+    my $dwo_dots_base = $base;
+
+    while ($link =~ /^\.\.\//) {
+      $link =~ s/^\.\.\///;
+      $dwo_dots_base =~ s/[\.\-_\w]+\/$//;
+    }
+
+    if ($dwo_dots_base =~ /$self->origin_domain/) {
+      $href = $dwo_dots_base . $link;
+    } else {
+      $href = 'http://' . $self->origin . '/' . $link;
+    }
+  }
+  else {
+    # get rif of './'
+    $link =~ s/^\.\///;
+    $href = $base . $link;
+  }
+
+  $self->_add_new_link($href, $depth);
+}
+
+sub _remove_link_garbage {
+  my $link = shift;
+  $link =~ s/\#.*$//;
+  $link =~ s/^\s+//;
+  $link =~ s/[\r\n]+//g;
+
+  foreach my $unwanted_param ('share', 'link') {
+    if ( $link =~ /[\?\&]${unwanted_param}=/ ) {
+      $link =~ s/${unwanted_param}=.*&//g;
+      $link =~ s/${unwanted_param}=.*$//g;
+    }
+  }
+
+  $link =~ s/&$//g;
+
+  return $link;
+}
+
+sub get_base {
+  my ($base) = @_;
+
+  # get url til the first slash
+  $base =~ s#^(http:\/\/.+/).*#$1#;
+
+  if ($base !~ /\/$/) {
+    $base = $base . '/';
+  }
+
+  return $base;
+}
+
+sub _handle_link {
+  my ($self, $link, $base, $depth) = @_;
+
+  if ($link !~ /^http/ ) {
+    $self->_handle_non_http_link($link, $base, $depth);
+  }
+  else {
+    my $origin_domain = $self->origin_domain;
+
+    if ($link =~ /^https?:\/\/(www\.)?[a-z\.\-]*${origin_domain}/i) {
+      $self->_add_new_link($link, $depth);
+    }
+  }
+}
+
+sub add_links_from_root {
+  my ($self, $depth, $url) = @_;
+
+  my @anchors = $self->root->find('a');
+
+  # base is for concatenating links like 'journal.html'
+  my $base = get_base($url);
+
+  foreach (@anchors) {
+    my $link = $_->attr('href');
+
+    if ($link) {
+      $link = _remove_link_garbage($link);
+
+      if ($link =~ /^(mailto|javascript):/i
+        ||
+        # there can be also space in the end
+        $link =~ /\.(mp3|mp4|avi|bmp|gif|jpg|jpeg|zip|rar|msi|exe|png|gz|bz2|tar|swf|pdf|wav|asf|tgz|wmv|flv|rm|mpg)\s?$/i
+      ) {
+        next;
+      }
+
+      $self->_handle_link($link, $base, $depth);
+    }
+  }
+}
+
+sub check_website {
+  my ($self, $url) = @_;
+  my $content_with_meta_keywords = $self->get_content_with_meta_keywords();
+
+  my $is_alerted = 0;
+  my @matched_keywords = ();
+  foreach (@{$self->keywords}) {
+    if ( $content_with_meta_keywords  =~ /$_/i ) {
+      push @matched_keywords, $_;
+      if (!exists($self->allowed_keywords->{$_})) {
+        $is_alerted = 1;
+      }
+    }
+  }
+
+  $self->add_text("ALERT $url\n") if $is_alerted;
+
+  my $keyword_count = scalar @matched_keywords;
+
+  if ($keyword_count > 0) {
+    $self->add_text("possible bad content $url @matched_keywords\n");
+    $self->add_text("found keywords: $keyword_count\n\n");
+  }
+
+  return $is_alerted;
+}
+
+sub fetch_website {
+  my ($self, $url, $want_spider, $depth) = @_;
+
+#  print Dumper $self->links;
+  $self->root(0);
+  $self->get_root($url);
+
+  if (!$self->root) {
+    return 0;
+  }
+
+  if ($want_spider) {
+    $self->add_links_from_root($depth, $url);
+  }
+
+  return $self->check_website($url);
+}
+
+sub spider_website {
+  my ($self) = @_;
+
+  my $start = time;
+  print "TIME".$start."\n";
+
+  # fetch initial website
+  my $want_spider = 1;
+  my $max_depth = 1;
+  $self->alerted(
+    $self->fetch_website($self->website, $want_spider, $max_depth));
+
+  print "PO:\n";
+  print Dumper $self->links;
+# ma is_already_crawled ked tam uz mozu byt linky ktore maju vacsiu depth ako 1 ?
+# ale z tej jednej startovacej url nemozu by linky s depth vacsie ako 1
+
+  my @keys = keys %{$self->links};
+
+  # checks if there are other urls than initial
+  if (@keys && !$self->is_already_crawled()) {
+
+    # website is spidered to depth = 3
+    # referrer to depth = 1
+    $want_spider = $self->links->{$self->website}{want_spider} || 0;
+    $max_depth = ($want_spider) ? 3 : 1;
+
+    print "MAAAAAAX ". $self->website ." $max_depth\n";
+
+    for (my $depth = 1; $depth <= $max_depth; $depth++) {
+      print "DEPTH".$depth."\n";
+
+      $self->proceed($start, $depth, $want_spider);
+    }
+  }
+
+  return;
+}
+
+sub proceed {
+  my ($self, $start, $depth, $want_spider) = @_;
+
+  # fetch_website adds urls to links
+  my @keys = keys %{$self->links};
+  foreach (@keys) {
+    if ($self->links->{$_}{depth} == ($depth) && not defined $self->links->{$_}{fetched}) {
+      # skip forums
+      if ($_ =~ /forums?\/index.php/) {
+        $self->add_text("SKIPPING $_\n");
+        $self->links->{$_}{fetched} = 1;
+        next;
+      }
+
+      if ( (time - $start) > 120 ) {
+        $self->add_text("TIMEOUT $self->website, number of links = ". (scalar @keys) ."\n");
+        last;
+      }
+      else {
+        print "fetching $_\n";
+        $self->add_text("fetching $_\n");
+      }
+
+      my $returned = $self->fetch_website($_, $want_spider, $depth+1);
+      $self->links->{$_}{fetched} = 1;
+      if ($returned) {
+        $self->alerted(1);
+      }
+    }
+  }
+
+  return;
+}
+
+sub settle_website {
+  my ($self, $website) = @_;
+
+  $self->links->{$website}{fetched} = 1;
+
+  print "SPIDER ".$website."\n";
+  $self->add_text("\nSPIDER ".$website."\n");
+
+  if ( $website !~ /http/ ) {
+    $website = 'http://' . $website;
+  }
+
+  $self->website($website);
+
+  # origin* may be different for website and for referrer
+  my ($origin, $origin_domain) = find_origin($website);
+  $self->origin($origin);
+  $self->origin_domain($origin_domain);
+
+  print "ORIGIN". $origin ."\n";
+  print "DOMAIN". $origin_domain ."\n";
+
+  print "PRED:\n";
+  print Dumper $self->links;
+
+  return;
+}
+
+sub spider_links {
+  my ($self) = @_;
+
+  $self->add_text("SPIDER LINKS\n");
+#  print Dumper $self->links;
+
+  my @zero_keys = keys %{$self->links};
+
+  foreach (@zero_keys) {
+#    print Dumper $self->links->{$_}{depth};
+#  print "ZERO " . $_ ."\n";
+    $self->settle_website($_);
+    $self->spider_website();
+    $self->counted(1) if ($self->alerted);
+  }
+
+  if ($self->counted) {
+    $self->add_text("this IS counted as alerted\n\n");
+  } else {
+    $self->add_text("this IS NOT counted as alerted\n\n");
+  }
+
+  print {$self->output_file()} $self->output;
+  print {$self->output_file()} "----------------------------------------------------------------------\n\n";
+
+  return $self->counted;
+}
+
+1;
